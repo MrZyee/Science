c8d9750da975ec114361d73bcbc473de7588d75a
Bartłomiej Koceniak <bartlomiej.koceniak@remog.onmicrosoft.com>
2023-03-22 09:03:05 +0100

    Creating the mechanics of the game

---
+++ Snake/Snake/Meal.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    public class Meal
+    {
+        public Meal()
+        {
+            Random rand = new Random();
+            var x =rand.Next(1, 20);
+            var y =rand.Next(1, 20);
+            CurrentTarget = new Coordinate(x, y); // przypisanie do propertisa z tego powodu że będzemy potrzebowali na zewnątrz zobaczyć jaki jest ten CurrentTarget żeby zrobic sprawdzenie w grze czy nasz snake zjadł ten posiłek
+            Draw();
+        }
+
+        public Coordinate CurrentTarget { get; set; }
+
+        void Draw()
+        {
+            Console.SetCursorPosition(CurrentTarget.X, CurrentTarget.Y);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("$");
+        }
+    }
+}
---
+++ Snake/Snake/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Snake
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool exit = false;
+            double frameRate = 1000 / 5.0;
+            DateTime lastDate = DateTime.Now;
+            Meal meal = new Meal();
+            //game loop
+            while (!exit)
+            {
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo input = Console.ReadKey();  //odczytanie klawisza od użytkownika
+
+                    switch (input.Key)
+                    {
+                        case ConsoleKey.Escape:
+                            exit = true;
+                            break;
+                        case ConsoleKey.LeftArrow:
+                            //x
+                            break;
+                        case ConsoleKey.RightArrow:
+                            //x
+                            break;
+                        case ConsoleKey.UpArrow:
+                            //x
+                            break;
+                        case ConsoleKey.DownArrow:
+                            //x
+                            break;
+                    }
+                }
+
+                if ((DateTime.Now - lastDate).TotalMilliseconds >= frameRate)
+                {
+                    // game action
+                    lastDate = DateTime.Now;
+                }
+            }
+        }
+    }
+}
